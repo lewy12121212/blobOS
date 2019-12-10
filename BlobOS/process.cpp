33cb0e9467b8c6306433f44187e694f99af26f73
purpleturtll <purpleturtll@gmail.com>
2019-12-10 11:51:08 +0100

    PCB z poprawkami Michała

--- BlobOS/process.cpp
+++ BlobOS/process.cpp
@@ -5,32 +5,37 @@
 #include "process.h"
 using namespace std;
 
-
-void PCB::copy_register(array<int, 5> &cpu_register){ //kopiowanie zawartości rejestru do PCB
-        for(int i=0; i<5; i++){
-            this->copy_register[i] = cpu_register[i];
-        }
+void PCB::copy_register(array<int, 5> &cpu_register)
+{ //kopiowanie zawartości rejestru do PCB
+    for (int i = 0; i < 5; i++)
+    {
+        this->register_copy[i] = cpu_register[i];
     }
-
-void PCB::restore_register(array<int, 5> &cpu_register){ // przywracanie zawartości rejestru
-        for(int i=0; i<5; i++){
-            cpu_register[i] = this->copy_register[i];
-        }
-    }
-
-void PCB::kill(){
-
-    this->change_state(terminated);
 }
 
-void PCB::change_state(process_state &new_state){
+void PCB::restore_register(array<int, 5> &cpu_register)
+{ // przywracanie zawartości rejestru
+    for (int i = 0; i < 5; i++)
+    {
+        cpu_register[i] = this->register_copy[i];
+    }
+}
+
+void PCB::kill()
+{
+    process_state state = terminated;
+    this->change_state(state);
+}
+
+void PCB::change_state(process_state &new_state)
+{
     this->state = new_state;
 }
 
-void create_process_size(string &name, int &size, int &ppid){
-    
+void create_process_size(string &name, int &size, int &ppid)
+{
 }
 
-void create_process_file(string &name, string &file_name){ // zakładająć że nie mamy podfolderów i ścieżka będzię jedynie nazwą pliku
-
+void create_process_file(string &name, string &file_name)
+{ // zakładająć że nie mamy podfolderów i ścieżka będzię jedynie nazwą pliku
 }
--- BlobOS/process.h
+++ BlobOS/process.h
@@ -5,29 +5,38 @@
 #include <iostream>
 #include <memory>
 using namespace std;
+static int pid_pcb = 0;
 
-int pid_pcb = 0;
-enum process_state{ ready, run, wait, terminated }; // terminated?
+enum process_state
+{
+    ready,
+    run,
+    wait,
+    terminated
+}; // terminated?
 
-class PCB{
+class PCB
+{
 
-protected:
+public:
     string name;
     int pid;
-    int ppid;    // pid rodzica 
-    int time_run; //przydzielony kwant czasu do wykonania
-    process_state state; // stan procesu
-    array<int, 5> register_copy{0,0,0,0,0}; // kopia rejestru
+    int ppid;                                   // pid rodzica
+    int time_run;                               //przydzielony kwant czasu do wykonania
+    process_state state;                        // stan procesu
+    array<int, 5> register_copy{0, 0, 0, 0, 0}; // kopia rejestru
     shared_ptr<PCB> parent_pcb = nullptr;
 
-    PCB(){ // konstruktor INIT
+    PCB()
+    { // konstruktor INIT
         this->name = "init";
         this->pid = 1;
         this->ppid = 0;
         this->state = ready;
     }
 
-    PCB(string &name, int &ppid){
+    PCB(string &name, int &ppid)
+    {
         this->name = name;
         pid_pcb++;
         this->pid = pid_pcb;
@@ -35,12 +44,11 @@ protected:
         this->state = ready;
     }
 
-    void copy_register(array<int, 5> &cpu_register); //kopiowanie zawartości rejestru do PCB
+    void copy_register(array<int, 5> &cpu_register);    //kopiowanie zawartości rejestru do PCB
     void restore_register(array<int, 5> &cpu_register); // przywracanie zawartości rejestru
-    void change_state(process_state &new_state); // zmiana stanu procesu
-    void kill(); //zabicie procesu
-
+    void change_state(process_state &new_state);        // zmiana stanu procesu
+    void kill();                                        //zabicie procesu
 };
 
-//void create_process_size(string &name, int &size, int &ppid); 
+//void create_process_size(string &name, int &size, int &ppid);
 void create_process_file(string &name, string &file_name, int &ppid); // zakładająć że nie mamy podfolderów i ścieżka będzię jedynie nazwą pliku
