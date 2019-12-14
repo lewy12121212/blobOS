e6fffc0faf54ec6944e366be5bcd12151ccfc3d0
Bartosz <56341461+bartoszcz616@users.noreply.github.com>
2019-12-14 16:41:45 +0100

    Wstępny szkielet RAMu; Kompiluje się ale funkcje w większości nic nie robią
    Wstawiam w tym stanie, żeby było mniej-więcej widać, jak ten RAM będzie wyglądać i żebyście nie musieli na mnie ze wszystkim czekać aż tak długo (tylko trochę krócej z częścią rzeczy). Póki co nie jest funkcjonalne i prawie na pewno funkcje się pozmieniają.

---
+++ BlobOS/Memory.cpp
@@ -0,0 +1,68 @@
+﻿#include "Memory.h"
+
+//Metody struktury PageInfo
+
+PageInfo::PageInfo()
+{
+	this->bit = false;
+	this->frame = -1;
+};
+
+PageInfo::PageInfo(int frame, bool bit)
+{
+	this->bit = bit;
+	this->frame = frame;
+}
+
+//Metody użytkowe
+
+Memory::Memory()
+{
+	for (char &e : RAM)
+		e = ' ';
+}
+
+void Memory::write_to_ram(int address, char* data)
+{
+
+}
+
+void Memory::write_instruction(std::shared_ptr<std::vector<PageInfo>> pt, char* data)
+{
+
+}
+
+void Memory::show_frame(int nr)
+{
+
+}
+
+std::shared_ptr<std::vector<PageInfo>> Memory::create_page_table(int size, int pid)
+{
+	return nullptr;
+}
+
+//Metody pracy krokowej i wyświetlania pamięci
+
+void Memory::show_ram()
+{
+	char c;
+	std::cout << "RAM content:";
+	for (int i = 0; i < 16; i++)
+	{
+		std::cout << std::endl << "frame " << i << ":\t";
+		for (int j = 0; j < 16; j++)
+		{
+			c = RAM[16 * i + j];
+			if (c == ' ')
+				std::cout << '_';
+			else
+				std::cout << c;
+		}
+	}
+}
+
+void Memory::show_pages(int pid)
+{
+
+}
---
+++ BlobOS/Memory.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include <vector>
+#include <memory>
+#include <iostream>
+
+/*Struktura z informacją o tym, czy dana strona znajduje się w RAM i jaką ramkę zajmuje. Wykorzystywana do tablicy stronic w PCB.*/
+struct PageInfo {
+	int frame; //Numer ramki, którą zajmuje dana stronica
+	bool bit;  //Wartość mówiąca, czy stronica znajduje się w RAM
+
+	PageInfo();
+	PageInfo(int frame, bool bit);
+};
+
+class Memory
+{
+	//Zmienne
+public:
+	char RAM[256] = {' '};
+
+	//Metody użytkowe
+
+	Memory();
+
+	/*Zapisuje dane do RAMu*/
+	void write_to_ram(int address, char *data);
+
+	/*Zapisuje rozkaz do RAMu*/
+	void write_instruction(std::shared_ptr<std::vector<PageInfo>> pt, char *data);
+
+	/*Wyświetla daną ramkę.*/
+	void show_frame(int nr);
+
+	/*Tworzy wskaźnik do tablicy stronic procesu znajdującej się w PCB.
+	Używana przy tworzeniu nowego procesu.
+	size - potrzebna ilość bajtów
+	pid  - ID procesu*/
+	std::shared_ptr<std::vector<PageInfo>> create_page_table(int size, int pid);
+
+	//Metody pracy krokowej i wyświetlania pamięci
+	
+	/*Wyświetla cały RAM.*/
+	void show_ram();
+
+	/*Wyświetla strony danego procesu.*/
+	void show_pages(int pid);
+};
