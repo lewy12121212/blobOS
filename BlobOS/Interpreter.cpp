f68d0f86b5898d7b785b1ad55747d5663fd16d8d
luki981259 <luki981259@gmail.com>
2020-01-09 15:12:48 +0100

    prawie wszystkie rozkazy z wyj. OF i CF

--- BlobOS/Interpreter.cpp
+++ BlobOS/Interpreter.cpp
@@ -8,7 +8,8 @@
 using namespace std;
 
 extern FileManager FM;
-
+extern ProcTree PTree;
+extern Memory memory;
 
 Interpreter interpeter;
 
@@ -73,6 +74,8 @@ int Interpreter::execute_instruction(std::string& instruction, shared_ptr<PCB>& 
 	int i = 0;
 	int adres = 0;
 
+	char val;
+
 	int *rej1=0;
 	int *rej2=0;
 	int counter=0;
@@ -81,6 +84,8 @@ int Interpreter::execute_instruction(std::string& instruction, shared_ptr<PCB>& 
 
 	string command;
 	string file_name;
+	string text;
+
 	command = exec_instruction[0];
 
 
@@ -106,6 +111,7 @@ int Interpreter::execute_instruction(std::string& instruction, shared_ptr<PCB>& 
 
 		}
 		else {
+			val = exec_instruction[1].c_str;
 			i = stoi(exec_instruction[1]);
 			*rej1 = i;
 		}
@@ -129,10 +135,11 @@ int Interpreter::execute_instruction(std::string& instruction, shared_ptr<PCB>& 
 			exec_instruction[1].pop_back();
 			exec_instruction[1].erase(exec_instruction[1].begin());
 
-			file_name = exec_instruction[1];
+			text = exec_instruction[1];
 
 		}
 		else {
+			val = exec_instruction[2].c_str;
 			i = stoi(exec_instruction[2]);
 			*rej2 = i;
 		}
@@ -154,9 +161,11 @@ int Interpreter::execute_instruction(std::string& instruction, shared_ptr<PCB>& 
 	else if (command == "MV") { *rej1 = *rej2; }
 	else if (command == "WR") {
 		//tutaj pisanko do pamiêci
+		memory.write_to_ram(adres, &val);
 	}
 	else if (command == "GT") {
 		//pobieranko z pamiêci
+		memory.get_frame(adres);
 	}
 	else if (command == "JP") {
 		instruction_counter = adres;
@@ -179,23 +188,29 @@ int Interpreter::execute_instruction(std::string& instruction, shared_ptr<PCB>& 
 		//tu te¿
 	}
 	else if (command == "WF") {
+
+		FM.save_data_to_file(file_name, text);
 		//i tu
 	}
 	else if (command == "AF") {
-		//a tak¿e tu
+		FM.edit_file(file_name, text);
 	}
 	else if (command == "RF") {
+
+		FM.show_file(file_name);
 		//kurwa ile jeszcze 
 	}
 	else if (command == "CF") {
 		//dobra koniec xd	
 	}
 	else if (command == "CP") {
+		PTree.create_process_file(file_name, text, running_proc->pid);
 		//tu teoretycznie moge stworzyc proces
 	}
 	else if (command == "KP") {
 		//a tu go zabic 
 		running_proc->kill();
+		//blaa
 	}
 	else if (command == "HT") {
 
